ade925b25579104f8e8d97e5ae3596a64ded7526
kaierih <kai.e.hoff@ntnu.no>
2021-01-29 08:11:17 +0100

    Oppdatert Frekvensrepresentasjon
    Endringer i forelesningsslides i forkant av forelesning fredag 29. Januar

--- Kildekode/_04_Frekvensrepresentasjon.py
+++ Kildekode/_04_Frekvensrepresentasjon.py
@@ -6,13 +6,13 @@ from ipywidgets import interact, fixed, FloatSlider, IntSlider, HBox, VBox, inte
 import ipywidgets as widget
 
 # Funksjon brukt i eksempler om frekvensforskyvning
-def displayDualSpectrum(x, fs):
+def displayDualSpectrum(x, fs, color=None, label=None, linestyle=None):
     N = len(x)
     Xk = np.fft.fft(x)/N
     Xk = np.fft.fftshift(Xk)
     f = np.array(np.arange(-fs/2, fs/2, fs/N))
     plt.xlim([-fs/2, fs/2])
-    plt.plot(f, np.abs(Xk))
+    plt.plot(f, np.abs(Xk), color=color, label=label, linestyle=linestyle)
     plt.xlabel("Frekvens (Hz)")
     plt.grid(True)
     plt.ylim(ymin=0)
@@ -145,8 +145,9 @@ def getCross(x, y, x_size=0.1, y_size=0.1):
     return [x_cross, y_cross]
 
 # UI tool to inspect segments of a larger signal in time and frequency domain   
+# UI tool to inspect segments of a larger signal in time and frequency domain   
 class FourierDemo:
-    def __init__(self, x_t, f_s, max_k=7, fig_num=1):
+    def __init__(self, x_t, f_s, max_k=10, fig_num=1):
         self.L = len(x_t)
         self.r_t = np.absolute(x_t)
         self.phi_t = np.angle(x_t)
@@ -154,16 +155,19 @@ class FourierDemo:
         self.t = np.linspace(0, 1, self.L)
         self.mixer = 2*pi*self.t
         self.k = 0
+        self.k_vect = np.arange(-max_k, max_k+ 1)
         self.animationSteps = np.linspace(1/20, 1, 20)
         self.a_k = []
         self.max_k = max_k
+        self.coeff_win = np.array([-3, 3])
         
         for i in range(-max_k, max_k+1):
             self.a_k.append(np.mean(x_t*np.exp(-2j*pi*self.t*i)))
+        self.a_k = np.array(self.a_k)
         
         # Set up canvas
         plt.close(fig_num)
-        self.fig = plt.figure(fig_num, figsize=(9, 6))
+        self.fig = plt.figure(fig_num, figsize=(12, 8))
         
         
         # Set up plot showing signal
@@ -182,42 +186,57 @@ class FourierDemo:
         
         # Set up plot showing magnitude spectrum
         self.ax2 = plt.subplot(2,3, 3)
-        self.ax2.stem(np.arange(-max_k, max_k+1), # Samplenummer
-                      np.absolute(self.a_k), # Signalverdier gitt samplenummer (x[n])
-                      linefmt='-.', # Linjestil stolper
-                      markerfmt='o', # Punktstil for stem-markere. Default er 'o' (stor prikk)
-                      basefmt='grey', # Farge på y=0 aksen
-                      use_line_collection=True # Hvordan "stem" skal håndtere dataene. Bruk alltid True.
-                      )
-        self.ax2.set_ylim(ymin=0)
+        self.ampCoeffMarker, = self.ax2.plot([0, 0], [0, 10], ':', color='tab:red', linewidth=2)
+        self.ax2.set_ylim([0, round(max(abs(self.a_k))+0.1,1)])
         self.ax2.set_xlim([-max_k, max_k])
-        self.ax2.set_xticks(np.arange(-max_k, max_k+1))
+        self.ax2.set_xticks(self.k_vect)
+        self.ax2.set_xticklabels([r'$\frac{'+str(i)+'}{T_0}$' for i in self.k_vect])
+        self.ax2.set_xlim(self.coeff_win)
         self.ax2.grid(True)
         self.ax2.set_ylabel(r'$A_k$')
-        x, y = getDiamond(0, np.absolute(self.a_k[max_k]), 0.5, 0.05)
-        self.ampCoeff, = self.ax2.plot(x, y, color='tab:red')
+        self.ax2.set_xlabel("Frekvens")
 
-
+        self.ax2b = self.ax2.twiny()
+        self.ampCoeff = self.ax2b.stem(self.k_vect, # Samplenummer
+                                      np.absolute(self.a_k), # Signalverdier gitt samplenummer (x[n])
+                                      linefmt='-C0', # Linjestil stolper
+                                      markerfmt='.', # Punktstil for stem-markere. Default er 'o' (stor prikk)
+                                      basefmt='grey', # Farge på y=0 aksen
+                                      use_line_collection=True # Hvordan "stem" skal håndtere dataene. Bruk alltid True.
+                                      )
+        self.ax2b.set_ylim(ymin=0)
+        self.ax2b.set_xticks(self.k_vect)
+        self.ax2b.set_xlim(self.coeff_win)
+        self.ax2b.set_xlabel("Koeffisient-nummer 'k'")
         
         # Set up plot showing phase spectrum
         self.ax3 = plt.subplot(2,3, 6)
-        self.ax3.stem(np.arange(-max_k, max_k+1), # Samplenummer
-                      np.angle(self.a_k), # Signalverdier gitt samplenummer (x[n])
-                      linefmt='-.', # Linjestil stolper
-                      markerfmt='o', # Punktstil for stem-markere. Default er 'o' (stor prikk)
-                      basefmt='grey', # Farge på y=0 aksen
-                      use_line_collection=True # Hvordan "stem" skal håndtere dataene. Bruk alltid True.
-                      )        
+        self.angleCoeffMarker, = self.ax3.plot([0, 0], [-pi, pi], ':', color='tab:red', linewidth=2)
         self.ax3.set_ylim([-np.pi, np.pi])
-        self.ax3.set_xticks(np.arange(-max_k, max_k+1))
+        self.ax3.set_xticks(self.k_vect)
         self.ax3.set_xlim([-max_k, max_k])
+        self.ax3.set_xticklabels([r'$\frac{'+str(i)+'}{T_0}$' for i in self.k_vect])
         self.ax3.set_yticks(np.linspace(-np.pi, np.pi, 5))
         self.ax3.set_yticklabels([r'$-\pi$', r'$-\frac{\pi}{2}$', r'$0$', r'$\frac{\pi}{2}$', r'$\pi$'])
         self.ax3.grid(True)
-        self.ax3.set_xlabel('k')
         self.ax3.set_ylabel(r'$\phi_k$')
-        x, y = getDiamond(0, np.angle(self.a_k[max_k]), 0.5, 0.4)
-        self.angleCoeff, = self.ax3.plot(x, y, color='tab:red')
+        self.ax3.set_xlim(self.coeff_win)
+        self.ax3.set_xlabel("Frekvens")
+        
+        self.ax3b = self.ax3.twiny()
+        self.angleCoeff = self.ax3b.stem(np.arange(-max_k, max_k+1), # Samplenummer
+                                         np.angle(self.a_k), # Signalverdier gitt samplenummer (x[n])
+                                         linefmt='-C0', # Linjestil stolper
+                                         markerfmt='.', # Punktstil for stem-markere. Default er 'o' (stor prikk)
+                                         basefmt='grey', # Farge på y=0 aksen
+                                         use_line_collection=True # Hvordan "stem" skal håndtere dataene. Bruk alltid True.
+                                         )        
+        self.ax3b.set_ylim([-np.pi, np.pi])
+        self.ax3b.set_xticks(self.k_vect)
+        self.ax3b.set_xlim(self.coeff_win)
+        self.ax3b.set_xlabel("Koeffisient-nummer 'k'")
+
+        
         
         # Adjust figure layout
         self.fig.tight_layout(pad=0.5, w_pad=1.0, h_pad=1.0)
@@ -228,7 +247,7 @@ class FourierDemo:
             min=-max_k,
             max=max_k,
             step=1,
-            description='Number of rotations "k":',
+            description="Antall Rotasjoner 'k':",
             disabled=False,
             style = {'description_width': 'initial'},
             #layout=Layout(width='95%'),
@@ -245,27 +264,34 @@ class FourierDemo:
         # Run demo
         #out = interactive_output(self.update, self.sliders)
         #display(self.layout, out)
+
         
     def update(self, k):
-        
+        self.coeffSquare.set_color('w')
+        self.coeffSquare.set_linewidth(0)
+      
         for i in (self.animationSteps*(k-self.k)+self.k):
+            # Påfør rotasjon på kurven
             phi_t = self.phi_t-self.mixer*i
             self.signalCurve.set_xdata(np.cos(phi_t)*self.r_t)
             self.signalCurve.set_ydata(np.sin(phi_t)*self.r_t)
-            #self.fig.tight_layout(pad=0.1, w_pad=1.0, h_pad=2.0)
+            
+            # Forskyv amplitudevisningen til koeffisientene
+            self.ax2b.set_xlim(self.coeff_win+i)
+            
+            # Forskyv fasevisningen til koeffisientene
+            self.ax3b.set_xlim(self.coeff_win+i)
+            
+            # Oppdater figur
             self.fig.canvas.draw()
+            
         titleStr = r"$x(t)\cdot e^{-j\frac{2\pi \cdot" +str(k)+"}{T_0}\cdot t}$"
         self.ax1.set_title(titleStr, fontsize=18)
         self.k = k
         
-        x, y = getDiamond(k, np.angle(self.a_k[self.max_k+k]), 0.5, 0.4)
-        self.angleCoeff.set_xdata(x)
-        self.angleCoeff.set_ydata(y)
-        
-        x, y = getDiamond(k, np.absolute(self.a_k[self.max_k+k]), 0.5, 0.05)
-        self.ampCoeff.set_xdata(x)
-        self.ampCoeff.set_ydata(y)
-        
+      
         x, y = getCross(np.real(self.a_k[self.max_k+self.k]), np.imag(self.a_k[self.max_k+self.k]), 0.2, 0.2)
+        self.coeffSquare.set_color('tab:red')
+        self.coeffSquare.set_linewidth(2)
         self.coeffSquare.set_xdata(x)
         self.coeffSquare.set_ydata(y)
